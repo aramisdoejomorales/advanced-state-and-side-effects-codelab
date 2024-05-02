559ac3b371d36717076a2e5c7933d775d3adc120
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-05-01 23:23:56 -0400

    Add .

--- app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
+++ app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
@@ -24,9 +24,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.samples.crane.ui.captionTextStyle
 import androidx.compose.ui.graphics.SolidColor
+
+class EditableUserInputState(private val hint: String, initialText: String) {
+    private var text: String by mutableStateOf(initialText)
+
+    /*fun updateText(newText: String) {
+        text = newText
+    }*/
+
+    val isHint: Boolean
+        get() = text == hint
+
+    companion object {
+        val Saver: Saver<EditableUserInputState, *> = listSaver(
+            save = { listOf(it.hint, it.text) },
+            restore = {
+                EditableUserInputState(
+                    hint = it[0],
+                    initialText = it[1]
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun rememberEditableUserInput(hint: String): EditableUserInputState =
+    rememberSaveable(hint, saver = EditableUserInputState.Saver) {
+        EditableUserInputState(
+            hint,
+            hint
+        )
+    }
 
 @Composable
 fun CraneEditableUserInput(
